5510ed7938c5f080111d555ca38c51424fcb4996
luckarts <luc.bachelerieart@gmail.com>
2020-07-16 16:47:05 +0200

    display listTopic linking with search result

--- pages/search/[...slug].js
+++ pages/search/[...slug].js
@@ -1,5 +1,45 @@
-function Search() {
-  return <div className="flex flex-col bg-gray-100">search Page</div>;
+import { GraphQLSchema, graphql } from 'graphql';
+import { QueryObjectType } from '../../schemaGraphqlReddit/reddit';
+import ListArticle from '../../components/ListArticle';
+
+function Search({ posts, slug, defaultFilter }) {
+  console.log(posts);
+  const filter = slug.length === 2 ? slug[1] : defaultFilter;
+  return (
+    <div className="flex flex-col">
+      {' '}
+      {posts.data.subreddit.topListings.map((post, index) => (
+        <ListArticle key={index} post={post} />
+      ))}
+    </div>
+  );
 }
 
+export async function getServerSideProps(context) {
+  const { slug } = context.params;
+  const defaultFilter = 'topListings';
+  let schema = new GraphQLSchema({
+    query: QueryObjectType
+  });
+
+  let query = `{ subreddit(name: "${slug[0]}"){
+        ${slug[1] ? slug[1] : defaultFilter}(limit: 2) {
+            title
+            text
+            score
+            numComments
+            createdISO
+            permalink
+            author {
+              username
+            }
+        }}}`;
+
+  const res = await graphql(schema, query);
+  const posts = JSON.parse(JSON.stringify(res));
+
+  return {
+    props: { posts, slug, defaultFilter }
+  };
+}
 export default Search;
