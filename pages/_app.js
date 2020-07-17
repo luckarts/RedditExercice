67435d8be2f936f5716eb2e6399b865103614ce2
luckarts <luc.bachelerieart@gmail.com>
2020-07-17 17:51:46 +0200

    feat nprogress when navigate in another page

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
 import '../styles/index.css';
 import Navbar from '../components/Navbar';
 import { useRouter } from 'next/router';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => {
+  NProgress.done();
+});
+Router.events.on('routeChangeError', () => {
+  NProgress.done();
+});
+
 function App({ Component, pageProps }) {
   const router = useRouter();
   return (
