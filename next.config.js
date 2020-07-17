23d6796e9707c9afe552fdd9b53ed77ee001f75f
luckarts <luc.bachelerieart@gmail.com>
2020-07-17 18:28:05 +0200

    add icon favicon && manifest jso n

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 const withCSS = require('@zeit/next-css');
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public'
+  }
+});
 module.exports = withCSS({
   /* config options here */
 });
--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar';
 import { useRouter } from 'next/router';
 import NProgress from 'nprogress';
 import Router from 'next/router';
-
+import Head from 'next/head';
 Router.events.on('routeChangeStart', () => {
   NProgress.start();
 });
@@ -18,6 +18,23 @@ function App({ Component, pageProps }) {
   const router = useRouter();
   return (
     <div className="bg-gray-100 min-h-screen">
+      <Head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
+        <title>NextJs PWA GraphQL & Tailwind</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <link href="./favicons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+        <link href="./favicons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
+      </Head>
       {router.pathname !== '/' && <Navbar />}
 
       <Component {...pageProps} />
