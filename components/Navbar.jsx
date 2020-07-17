96238d4877ae767c9ac31543a078549028bf2b87
luckarts <luc.bachelerieart@gmail.com>
2020-07-17 18:00:41 +0200

    fix link home to logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,20 +1,23 @@
 import FormSearch from './FormSearch';
+import Link from 'next/link';
 const Nav = () => {
   return (
     <nav className="bg-teal-500 p-3">
       <div className="w-2/3 m-auto flex items-center justify-between flex-wrap sm:w-full">
-        <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <svg
-            className="fill-current h-8 w-8 mr-2"
-            width="54"
-            height="54"
-            viewBox="0 0 54 54"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-          </svg>
-          <span className="font-semibold text-xl tracking-tight">Reddit Exercice</span>
-        </div>
+        <Link href="/">
+          <a className="flex items-center flex-shrink-0 text-white mr-6">
+            <svg
+              className="fill-current h-8 w-8 mr-2"
+              width="54"
+              height="54"
+              viewBox="0 0 54 54"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+            </svg>
+            <span className="font-semibold text-xl tracking-tight">Reddit Exercice</span>
+          </a>
+        </Link>
         <div className=" relative flex">
           <FormSearch className="w-full text-sm shadow-md border-2 border-gray-300 bg-white h-10 px-6  pr-16 rounded-full  focus:outline-none" />
         </div>
