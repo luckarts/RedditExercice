22658e9a0292ba2fc714fae5ca613fa312ba9170
luckarts <luc.bachelerieart@gmail.com>
2020-07-30 15:29:26 +0200

    clean code add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,12 +11,10 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:jsx-a11y/recommended',
 
-    // Prettier plugin and recommended rules
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   rules: {
-    // Include .prettierrc.js rules
     'prettier/prettier': ['error', {}, { usePrettierrc: true }],
 
     'react/prop-types': 'off',
